650b32422b3d633a7d6ab2ef8ee663f2b7e095ee
Kaloyan Botev <eczenn@gmail.com>
2021-10-29 09:27:23 +0300

    buggy appp

--- api/index.js
+++ api/index.js
@@ -36,7 +36,7 @@ app.use('/api/categories',categoryRoute)
 
 app.use(express.static(path.join(__dirname, "/client/build")));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build','index.html'));
 });
 
