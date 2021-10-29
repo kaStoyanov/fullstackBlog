89829991e61ac46cee45ece556fbea04d957f4ae
Kaloyan Botev <eczenn@gmail.com>
2021-10-29 11:45:19 +0300

    a

--- api/index.js
+++ api/index.js
@@ -40,6 +40,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build','index.html'));
 });
 
-app.listen(poccess.env.PORT || 5000,()=>{
+app.listen( 5000,()=>{
     console.log('server`s up')
 })
